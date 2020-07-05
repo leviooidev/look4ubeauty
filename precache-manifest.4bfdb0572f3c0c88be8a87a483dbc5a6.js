2c94ed168b52d1dcedf4d7690e4396436c145781
Ooi Guan Lee <ooiguanlee@Oois-MacBook-Pro.local>
2020-07-05 10:15:55 +0800

    Updates

---
+++ precache-manifest.4bfdb0572f3c0c88be8a87a483dbc5a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715d68b20ecbc9ec3f0a60cff009b6f9",
+    "url": "/look4ubeauty/index.html"
+  },
+  {
+    "revision": "c5a3b3648da85a987bf9",
+    "url": "/look4ubeauty/static/css/main.2d2ee9b6.chunk.css"
+  },
+  {
+    "revision": "7e30cd1696d69774ea17",
+    "url": "/look4ubeauty/static/js/2.4f0d1a4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/look4ubeauty/static/js/2.4f0d1a4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a3b3648da85a987bf9",
+    "url": "/look4ubeauty/static/js/main.ab34b98b.chunk.js"
+  },
+  {
+    "revision": "3477cf04cb3220b642f1",
+    "url": "/look4ubeauty/static/js/runtime-main.9b6cf8da.js"
+  },
+  {
+    "revision": "1d2219c5174783edad2308220c4d0e7a",
+    "url": "/look4ubeauty/static/media/logo.1d2219c5.png"
+  }
+]);
