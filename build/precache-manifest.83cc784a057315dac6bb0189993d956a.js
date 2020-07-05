00febf609494ab5db640cd04f54ce2a4ffbefd33
Ooi Guan Lee <ooiguanlee@Oois-MacBook-Pro.local>
2020-07-05 21:08:40 +0800

    Deploy first maintainance version to nginx

---
+++ build/precache-manifest.83cc784a057315dac6bb0189993d956a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c560212cea7edd140dad57636f2a1e8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b2f0abc9e9547cb6ae0",
+    "url": "./static/css/main.2d2ee9b6.chunk.css"
+  },
+  {
+    "revision": "7e30cd1696d69774ea17",
+    "url": "./static/js/2.4f0d1a4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4f0d1a4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2f0abc9e9547cb6ae0",
+    "url": "./static/js/main.aa9873bc.chunk.js"
+  },
+  {
+    "revision": "e13e1df5ee6ec8a4556a",
+    "url": "./static/js/runtime-main.1147b1fd.js"
+  },
+  {
+    "revision": "1d2219c5174783edad2308220c4d0e7a",
+    "url": "./static/media/logo.1d2219c5.png"
+  }
+]);
