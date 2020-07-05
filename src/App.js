b24003486cc610af0625dac31d1e272c691525b7
Ooi Guan Lee <ooiguanlee@Oois-MacBook-Pro.local>
2020-07-05 10:15:13 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './image/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          The site is under maintainance
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
